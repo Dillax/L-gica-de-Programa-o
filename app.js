ffdad1b4f78e8409915ec58b8d359f496d44d2e3
Robert Simão dos Santos <163461719+Dillax@users.noreply.github.com>
2025-11-04 15:30:32 -0300

    atualizado

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-alert('Bem-vindo ao jogo do número secreto')
-let chute = prompt('Escolha um número entre 1 e 10')
+alert('Boas vindas ao jogo do número secreto');
+let numeroSecreto = 9;
+console.log(numeroSecreto)
+let chute = prompt('Escolha um número entre 1 e 10');
 
-let numeroSecreto = 4
-
-console.log(chute == numeroSecreto)
+// se chute for igual ao número secreto
 if (chute == numeroSecreto) {
-    alert('Acertou')
+    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
 } else {
-    alert('O número secreto era ' + numeroSecreto)
+    alert('Você errou :(')
 }
